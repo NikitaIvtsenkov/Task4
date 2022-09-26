b540b4bb85bc4b4df46a293db1a68cad1a97ee21
Nikita <102020471+NikitaIvtsenkov@users.noreply.github.com>
2022-09-26 14:46:01 +0300

    Update Task4.java

--- src/task4/Task4.java
+++ src/task4/Task4.java
@@ -5,6 +5,9 @@
  */
 package task4;
 
+import java.util.Arrays;
+import java.util.Random;
+
 /**
  *
  * @author pupil
@@ -15,7 +18,30 @@ public class Task4 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Random random = new Random();
+        int n = 101;
+        int sum = 0;
+        System.out.println("Одномерные Массивы");
+        int[] nums = new int[n];
+        for (int i = 0; i < nums.length; i++) {
+            while(true){
+                int rundNum = random.nextInt(100);
+                if (rundNum % 2 != 0){
+                    nums[i] = rundNum;
+                    break;
+                }
+            }
+        }
+        Arrays.sort(nums);    
+        for (int i = 1; i < nums.length; i++) {
+            System.out.printf("%-4d", nums[i]);
+            sum += nums[i];  
+            if (i%10==0){
+                System.out.println("");
+            }
+                
+        }
+        System.out.println();
+        System.out.println("Среднее арифметическое: " + sum/n);
     }
-    
 }
